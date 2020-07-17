591117c6e5a6f22d8f8f86ac71d69b00c1f4de33
azarrias <adolfo.zarrias@gmail.com>
2020-07-17 21:22:57 +0200

    Download tv show episode subtitles

--- argenteam_api_client.py
+++ argenteam_api_client.py
@@ -10,7 +10,8 @@ MOVIE_URL = API_URL + "movie"
 EPISODE_URL = API_URL + "episode"
 
 class Result(object):
-    def __init__(self, title, _type, summary, year=None):
+    def __init__(self, _id, title, _type, summary, year=None):
+        self.id = _id
         self.title = title
         self.type = _type
         self.summary = summary
@@ -40,16 +41,54 @@ def response_summary(num_results):
 def response_elements(response):
     elements = []
     for r in response['results']:
-        elements.append(Result(r['title'], r['type'], r['summary'], r.get('year')))
+        elements.append(Result(r['id'], r['title'], r['type'], r['summary'], r.get('year')))
     return elements
 
-def promptUser(options):
+def prompt_user(options, elements, output = None):
     for o in options:
         print(options[o])
     user_input = input("> ")
     if user_input == 'S' and "EXIT" in options:
         sys.exit(0)
-    return search(user_input)
+    elif user_input == 'D' and "SUBS" in options and output:
+        for season in output['seasons']:
+            for episode in season['episodes']:
+                dl_subs(episode['id'])
+    elif user_input.isdigit() and int(user_input) > 0 and int(user_input) <= len(elements) and "VIEW" in options:
+        result = elements[int(user_input) - 1]
+        if result.type == 'tvshow':
+            output = search_tvshow(result.id)
+        options = { "SEARCH": "Buscar por película, serie, actor o director", 
+            "SUBS": "[D]escargar subtítulos", 
+            "EXIT": "[S]alir" }
+    else:
+        output = search(user_input)
+        num_results = output['total']
+        print(response_summary(num_results))
+        if num_results > 0:
+            elements = response_elements(output)
+            for i in range(len(elements)):
+                print("[" + str(i + 1) + "] - " + str(elements[i]))
+            print()
+            options = { "SEARCH": "Buscar por película, serie, actor o director", 
+                "VIEW": "Número a la izquierda del título para ver el detalle", 
+                "EXIT": "[S]alir" }
+        
+    return options, elements, output
+
+def dl_subs(episode_id):
+    episode = search_episode(episode_id)
+    for release in episode['releases']:
+        if 'subtitles' in release:
+            for subs in release['subtitles']:
+                if 'uri' in subs:
+                    url = subs['uri']
+                    local_filename = url.split("/")[-1]
+                    r = requests.get(url, stream = True) # download streaming
+                    with open(local_filename, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size = 1024):
+	                        if chunk: # filter out keep-alive new chunks
+		                        f.write(chunk)
 
 def search(query):
     try:
@@ -60,22 +99,30 @@ def search(query):
         sys.exit(0)
     return response.json()
 
+def search_episode(id):
+    try:
+        response = requests.post(EPISODE_URL, data={"id": id})
+    except requests.exceptions.RequestException:
+        print("ERROR! No se ha podido obtener respuesta del servidor")
+        print("Revisa los datos de conexión")
+        sys.exit(0)
+    return response.json()    
+
+def search_tvshow(id):
+    try:
+        response = requests.post(TVSHOW_URL, data={"id": id})
+    except requests.exceptions.RequestException:
+        print("ERROR! No se ha podido obtener respuesta del servidor")
+        print("Revisa los datos de conexión")
+        sys.exit(0)
+    return response.json()
+
 if __name__ == '__main__':
     print("aRGENTeaM API client (podría requerir el uso de VPN)")
     print("====================================================")
     print()
-    options = { "SEARCH": "Buscar por película, serie, actor o director", "EXIT": "[S] para salir" }
+    options = { "SEARCH": "Buscar por película, serie, actor o director", "EXIT": "[S]alir" }
     elements = []
+    output = None
     while True:
-        output = promptUser(options)
-        num_results = output['total']
-        print(response_summary(num_results))
-        if num_results == 0:
-            continue
-        elements = response_elements(output)
-        for i in range(len(elements)):
-            print("[" + str(i + 1) + "] - " + str(elements[i]))
-        print()
-        options = { "SEARCH": "Buscar por película, serie, actor o director", 
-            "VIEW": "Número a la izquierda del título para ver el detalle", 
-            "EXIT": "[S] para salir" }
+        options, elements, output = prompt_user(options, elements, output)
